b41f6674358f2f5c990056ab60610c19db13886e
mordroreur <mlaurencot@gmail.com>
2023-10-15 05:07:22 +0200

    get demands and affiches them

--- Client/lib/main.dart
+++ Client/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cron/cron.dart';
 import 'package:flutter/material.dart';
 //import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -9,16 +10,19 @@ import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 
-final String IP = "192.168.246.89";
+final String IP = "192.168.160.89";
 
 int tenteMDP = 0;
+
+final dio = Dio();
+Directory appDocDir = Directory("");
+CookieJar cookieJar = CookieJar();
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({super.key});
 
   @override
@@ -27,7 +31,6 @@ class MyApp extends StatelessWidget {
       home: AuthenticationWrapper(),
     );
   }
-
 }
 
 class AuthenticationWrapper extends StatefulWidget {
@@ -36,13 +39,7 @@ class AuthenticationWrapper extends StatefulWidget {
 }
 
 class _AuthenticationWrapperState extends State<AuthenticationWrapper> {
-
   bool _isAuthenticated = false;
-
-  final dio = Dio();
-  Directory appDocDir = Directory("");
-  CookieJar cookieJar = CookieJar();
-  
 
   String calculateSHA256(String input) {
     final bytes = utf8.encode("Seeel" + input + "Poooiivre");
@@ -52,24 +49,27 @@ class _AuthenticationWrapperState extends State<AuthenticationWrapper> {
 
   @override
   void initState() {
+    var cron = Cron();
+    cron.schedule(Schedule.parse('*/1 * * * *'), () async {
+      print('every three minutes');
+    });
     super.initState();
     checkAuthentication();
   }
 
   Future<void> checkAuthentication() async {
     // Check if the user is authenticated (e.g., by checking a token, session, etc.)
-    appDocDir =  await getApplicationDocumentsDirectory();
-    String pth = Directory(appDocDir.path.toString() + "/.cookies/").path.toString();
-    print("Le sting : ");
-    print(pth);
-    print("fini");
-    cookieJar = PersistCookieJar(ignoreExpires: false, storage: FileStorage(pth));
-    dio.interceptors.add(CookieManager(cookieJar)); 
+    appDocDir = await getApplicationDocumentsDirectory();
+    String pth =
+        Directory(appDocDir.path.toString() + "/.cookies/").path.toString();
+    cookieJar =
+        PersistCookieJar(ignoreExpires: false, storage: FileStorage(pth));
+    dio.interceptors.add(CookieManager(cookieJar));
 
-    List<Cookie> results = await cookieJar.loadForRequest(Uri.parse("http://192.168.246.89:8080"));
+    List<Cookie> results =
+        await cookieJar.loadForRequest(Uri.parse("http://" + IP + ":8080"));
 
     //print(results.isEmpty);
-    
 
     setState(() {
       _isAuthenticated = !results.isEmpty;
@@ -80,16 +80,20 @@ class _AuthenticationWrapperState extends State<AuthenticationWrapper> {
     // Call your server's login endpoint to get the authentication token
     // Adjust the request body based on your server's requirements
     password = calculateSHA256(password);
-    
-    final response = await dio.post(
-      "http://192.168.246.89:8080/login",
-      queryParameters: {'username' : '$username', 'password' : '$password' },
-    );
-
+    Response response;
+    try {
+      response = await dio.post(
+        "http://" + IP + ":8080/login",
+        queryParameters: {'username': '$username', 'password': '$password'},
+      );
+    } catch (e) {
+      tenteMDP = 1;
+      setState(() {});
+      throw Exception('Failed to login');
+    }
 
     if (response.statusCode == 200) {
-
-      print(response.headers);
+      //print(response.headers);
       setState(() {
         _isAuthenticated = true;
       });
@@ -104,14 +108,14 @@ class _AuthenticationWrapperState extends State<AuthenticationWrapper> {
     // Call your server's logout endpoint TODO
     //await http.post(Uri.parse('your_api_endpoint/logout_endpoint'));
 
-    final response = await dio.get("http://192.168.246.89:8080/logout",
+    final response = await dio.get(
+      "http://" + IP + ":8080/logout",
       //Uri.http("192.168.246.89:8080", "/users"),
       //headers: {},
       /*headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },*/
       //body: // Update with actual password input
-      
     );
 
     //await _storage.delete(key: 'auth_token');
@@ -122,30 +126,153 @@ class _AuthenticationWrapperState extends State<AuthenticationWrapper> {
 
   @override
   Widget build(BuildContext context) {
-    return _isAuthenticated ? MainPage(logout: _logout) : LoginPage(login: _login);
+    return _isAuthenticated
+        ? MainPage(
+            menu: _getDemande,
+            logout: _logout,
+            resp: Response(requestOptions: RequestOptions()),
+          )
+        : LoginPage(login: _login);
   }
 }
+
+class MySeparator extends StatelessWidget {
+  const MySeparator({Key? key, this.height = 1, this.color = Colors.black})
+      : super(key: key);
+  final double height;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        final boxWidth = constraints.constrainWidth();
+        const dashWidth = 40.0;
+        final dashHeight = height;
+        final dashCount = (boxWidth / (2 * dashWidth)).floor();
+        return Flex(
+          children: List.generate(dashCount, (_) {
+            return SizedBox(
+              width: dashWidth,
+              height: dashHeight,
+              child: DecoratedBox(
+                decoration: BoxDecoration(color: color),
+              ),
+            );
+          }),
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          direction: Axis.horizontal,
+        );
+      },
+    );
+  }
+}
+
+class _getDemande {}
 
 class MainPage extends StatelessWidget {
   final Function logout;
 
-  MainPage({required this.logout});
+  MainPage({required menu, required this.logout, required this.resp});
+
+  final Response resp;
+
+  Response rep = Response(requestOptions: RequestOptions());
+
+  Future<bool> _getDBInfo() async {
+    rep = await dio.get(
+      "http://" + IP + ":8080/getMyDemandes",
+    );
+    //print(rep.toString());
+    return true;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Main Page'),
-      ),
-      body: Container(
-        color: Colors.white, // Page blanche
-        child: Center(
-          child: ElevatedButton(
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.logout_rounded),
+            tooltip: 'Déconection',
             onPressed: () {
               logout();
             },
-            child: Text('Déconnexion'),
           ),
+        ],
+        centerTitle: true,
+        title: const Text('Mail Drop'),
+      ),
+      body: Container(
+        color: Colors.white, // Page blanche
+        child: ListView(
+          padding: const EdgeInsets.all(8),
+          children: <Widget>[
+            Row(children: <Widget>[
+              Expanded(
+                //directions_boat_filled_outlined
+                child: IconButton(
+                  iconSize: 72,
+                  icon: const Icon(Icons.add_box_outlined),
+                  tooltip: 'Créer ???',
+                  onPressed: () {},
+                ),
+              ),
+            ]),
+            const MySeparator(color: Colors.grey),
+            const SizedBox(height: 10),
+            const Center(
+              child: Text(
+                "Mes Notifs",
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            const SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: () async {
+                if (await _getDBInfo()) {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => InformationMenu(resp: rep)));
+                }
+              },
+              /*
+              onPressed: () {
+                rep = _getDBInfo();
+                (Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => InformationMenu(Response: rep),
+                        ))) /*,
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => InformationMenu()),
+                )*/
+                    ;
+              },*/
+              child: const Text(
+                "Informaation",
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            const SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text(
+                "Action",
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            const SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text(
+                "Evènement",
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -174,7 +301,8 @@ class LoginPage extends StatelessWidget {
             if (tenteMDP == 1)
               Text(
                 'Mot de passe ou identifiant inconnu',
-                style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+                style:
+                    TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
               ),
             TextField(
               controller: usernameController,
@@ -200,10 +328,119 @@ class LoginPage extends StatelessWidget {
                 login(username, password);
               },
               child: Text('Login'),
-              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+/*
+class StatefulWrapper extends StatefulWidget {
+  final Function onInit;
+  final Widget child;
+  const StatefulWrapper({required this.onInit, required this.child});
+  @override
+  _StatefulWrapperState createState() => _StatefulWrapperState();
+}
+
+class _StatefulWrapperState extends State<StatefulWrapper> {
+  @override
+  void initState() {
+    if (widget.onInit != null) {
+      widget.onInit();
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+}
+*/
+
+class InformationMenu extends StatelessWidget {
+  Response resp = Response(requestOptions: RequestOptions());
+  InformationMenu({super.key, required this.resp});
+
+  constructWidFromString(BuildContext context) {
+    var allCont = <Widget>[];
+    var val = json.decode(resp.toString());
+
+    allCont.add(const SizedBox(height: 20));
+    //print(val.length);
+    for (int i = 1; i < val.length; i++) {
+      allCont.add(
+        ElevatedButton(
+          onPressed: () {
+            (Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => AfficheZoom(information: val[i]))));
+          },
+          child: Column(
+            children: [
+              Text(
+                val[i]['titre'].toString(),
+                style: const TextStyle(fontSize: 20),
+              ),
+              (val[i]['description'].toString().length < 51)
+                  ? Text(val[i]['description'].toString(),
+                      style: const TextStyle(fontSize: 10))
+                  : Text(
+                      val[i]['description'].toString().substring(0, 50) + "...",
+                      style: const TextStyle(fontSize: 10))
+            ],
+          ),
+        ),
+      );
+      allCont.add(const SizedBox(height: 20));
+    }
+
+    return allCont;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //final todo = ModalRoute.of(context).!.settings.arguments as Response;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Informations'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(8),
+        children: constructWidFromString(context),
+      ),
+    );
+  }
+}
+
+class AfficheZoom extends StatelessWidget {
+  var information;
+  AfficheZoom({super.key, required this.information});
+
+  @override
+  Widget build(BuildContext context) {
+    //final todo = ModalRoute.of(context).!.settings.arguments as Response;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Informations'),
+      ),
+      body: ListView(padding: const EdgeInsets.all(8), children: <Widget>[
+        const SizedBox(height: 20),
+        Center(
+            child: Text(
+          information['titre'],
+          style: TextStyle(fontSize: 26, color: Colors.red),
+        )),
+        Text(information['description'], style: TextStyle(fontSize: 15)),
+        const SizedBox(height: 50),
+        ElevatedButton(
+          child: Text("JSP"),
+          onPressed: () {},
+        )
+      ]),
     );
   }
 }
